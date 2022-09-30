d23ff33f463e4e089db4027d98f039e71a396832
gulzade <gulzadekrts@gmail.com>
2022-10-01 00:31:30 +0300

    creat xcode project

---
+++ TravelGuideApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelGuideApp
+//
+//  Created by Gülzade Karataş on 1.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
