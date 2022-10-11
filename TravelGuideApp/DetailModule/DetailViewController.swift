906fc970815298c291c233c6cf91a011a37f9c60
gulzade <gulzadekrts@gmail.com>
2022-10-11 04:04:04 +0300

    detail control of what to view

---
+++ TravelGuideApp/DetailModule/DetailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  DetailViewController.swift
+//  TravelGuideApp
+//
+//  Created by Gülzade Karataş on 10.10.2022.
+//
+
+import UIKit
+import Kingfisher
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var detailImageBackgroundView: UIView!
+    @IBOutlet weak var detailImageView: UIImageView!
+    @IBOutlet weak var detailCategoriesLabel: UILabel!
+    @IBOutlet weak var detailscrollView: UIScrollView!
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var detailTitle: UILabel!
+    var hotel: FlightCellViewModel?
+    var flight : FlightCellViewModel?
+    var article: Articles?
+    private var checkButton: Bool?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+        if flight != nil {
+            if let flight = flight {
+                detailCategoriesLabel.text = flight.category!
+                detailTitle.text = flight.airportName!
+                detailDescriptionLabel.text = "Departure ıata:  \(flight.departureIata!) \nArrival ıata:  \(flight.arrivalIata!)  \nTime:  \(flight.flightDate!) \nFlight number:  \(flight.flightNumber!)"
+            }
+        }
+        else if article != nil {
+            if let article = article {
+                detailImageView.kf.setImage(with: URL(string: article.articlesImageUrl!))
+                detailCategoriesLabel.text = "Article"
+                detailTitle.text = article.articlesTitle!
+                detailDescriptionLabel.text = article.articlesDescription!
+            }
+        }
+        setupUI()
+    }
+    
+    private func setupUI(){
+        
+        self.navigationController?.navigationBar.topItem?.backButtonTitle = ""
+        detailImageBackgroundView.layer.cornerRadius = 40
+        detailImageBackgroundView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        detailImageView.layer.cornerRadius = 40
+        detailImageView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+    }
+    
+    @IBAction func addBookmarkButton(_ sender: Any) {
+    }
+    
+}
