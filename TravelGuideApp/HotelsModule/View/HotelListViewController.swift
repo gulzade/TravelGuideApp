f00d08eef1f50f4c58090e794e9267b79ab0f2cc
gulzade <gulzadekrts@gmail.com>
2022-10-11 06:23:00 +0300

    creat Hotel viewModule

---
+++ TravelGuideApp/HotelsModule/View/HotelListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HotelListViewController.swift
+//  TravelGuideApp
+//
+//  Created by Gülzade Karataş on 11.10.2022.
+//
+
+import UIKit
+
+class HotelListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
